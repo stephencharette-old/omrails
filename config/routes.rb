3d63c44b38ed55384e1c8610e2cdbce0eed1819e
Stephen Charette <scharette@solutionstreet.com>
2021-03-06 18:58:51 -0500

    Added pages: Home, Contact Us, About, and added navigation header

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+  # get '/' => 'pages#home' this is the same as the above line
+  get 'about' => 'pages#about'
+  get 'contact-us' => 'pages#contact-us'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
