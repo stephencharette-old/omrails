74fa7405ecc883f8163b0368d711ce158b38ccf7
Stephen Charette <scharette@solutionstreet.com>
2021-03-06 19:13:22 -0500

    Added content to About, Contact, and Home and added contact to nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root 'pages#home'
   # get '/' => 'pages#home' this is the same as the above line
   get 'about' => 'pages#about'
-  get 'contact-us' => 'pages#contact-us'
+  get 'contact' => 'pages#contact-us'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
